41eba7d6975f4939725fa4ba71ffb46893c53985
thinking-weed <133303560+thinking-weed@users.noreply.github.com>
2023-09-13 19:28:55 +0900

    @vite復活

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
 
         {{-- 他のscripts --}}
 
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     </head>
     <body class="font-sans antialiased relative">
