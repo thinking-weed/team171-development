5f69548d51e3cf5a42abbc5b03a646101bd347a0
thinking-weed <133303560+thinking-weed@users.noreply.github.com>
2023-09-13 15:00:07 +0900

    ItemSeeder修正

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -13,12 +13,12 @@ class ItemSeeder extends Seeder
     public function run(): void
     {
         \App\Models\Item::create([
-            'user_id' => fake()->numberBetween($min=30,$max=100),//指定した範囲内の整数のどれか
-            'name' => fake()->realText(10),
-            'type' => fake()->numberBetween($min=1,$max=8),
-            'detail' =>fake()->realText(100),
-            'delete_flag' =>fake()->numberBetween($min=0,$max=1),
-            'updated_by' => fake()->numberBetween($min=1,$max=10),
+            'user_id' => '2',//指定した範囲内の整数のどれか
+            'name' => 'パン',
+            'type' => '1',
+            'detail' =>'3',
+            'delete_flag' =>'0',
+            'updated_by' => '1',
             'created_at' => now(),//php artisan db:seed --class=ItemsSeeder（<--今回のseeder名）をひたすら繰り返す
             'updated_at' => now(),//現在の時刻を入れるメソッド
             //なお、対応するmigrationファイルにtimestamps()のカラムがないとエラーになる
@@ -26,12 +26,12 @@ class ItemSeeder extends Seeder
         ]);
 
         \App\Models\Item::create([
-            'user_id' => fake()->numberBetween($min=30,$max=100),//指定した範囲内の整数のどれか
+            'user_id' => '3',//指定した範囲内の整数のどれか
             'name' => '地球破壊爆弾',//左のように文字列もmigrationファイルの条件を満たせば日本語も可能（シングルクォーテーションではさむ）
-            'type' => fake()->numberBetween($min=1,$max=8),
+            'type' => '9',
             'detail' =>'某猫型ロボットがネズミにどら焼きをたべられそうになった時に使用しかけた地球を破壊する力をもつ未来の爆弾',
             'delete_flag' => 1,
-            'updated_by' => fake()->numberBetween($min=1,$max=10),
+            'updated_by' => '1',
             'created_at' => now(),//php artisan db:seed --class=ItemsSeeder（<--今回のseeder名）をひたすら繰り返す
             'updated_at' => now(),//現在の時刻を入れるメソッド
             //なお、対応するmigrationファイルにtimestamps()のカラムがないとエラーになる
@@ -39,12 +39,12 @@ class ItemSeeder extends Seeder
         ]);//矢印の先のデータ型もマイグレーションファイルの設定に合わせる
 
         \App\Models\Item::create([
-            'user_id' => fake()->numberBetween($min=30,$max=100),//指定した範囲内の整数のどれか
-            'name' => fake()->realText(10),//realTextはなぜか宮沢賢治の「銀河鉄道の夜」から単語を拾うオプション
-            'type' => fake()->numberBetween($min=1,$max=8),
-            'detail' =>fake()->realText(100),
-            'delete_flag' =>fake()->numberBetween($min=0,$max=1),
-            'updated_by' => fake()->numberBetween($min=1,$max=10),
+            'user_id' => '4',//指定した範囲内の整数のどれか
+            'name' => '鉛筆',//realTextはなぜか宮沢賢治の「銀河鉄道の夜」から単語を拾うオプション
+            'type' => '2',
+            'detail' => '書くための道具',
+            'delete_flag' =>'0',
+            'updated_by' => '1',
             'created_at' => now(),//php artisan db:seed --class=ItemsSeeder（<--今回のseeder名）をひたすら繰り返す
             'updated_at' => now(),//現在の時刻を入れるメソッド
             //なお、対応するmigrationファイルにtimestamps()のカラムがないとエラーになる
@@ -52,13 +52,13 @@ class ItemSeeder extends Seeder
         ]);
 
         \App\Models\Item::create([
-            'user_id' => fake()->numberBetween($min=30,$max=100),//指定した範囲内の整数のどれか
+            'user_id' => '5',//指定した範囲内の整数のどれか
             'name' => '週刊少年ジャンプ',
             'type' => 6,
             'detail' =>'基本的に月曜日発売であるが、月曜日が祝日であるときにはその前の週の土曜日に発売する集英社の漫画雑誌、最近だと、あかね噺とwitchwatchが面白い',
             // witchwatchみたいな英語として認識されない文字があるときは、code spell checkerでその単語を無視する設定をしないとエラーになるので注意
             'delete_flag' => 0,
-            'updated_by' => fake()->numberBetween($min=1,$max=10),
+            'updated_by' => '1',
             'created_at' => now(),//php artisan db:seed --class=ItemsSeeder（<--今回のseeder名）をひたすら繰り返す
             'updated_at' => now(),//現在の時刻を入れるメソッド
             //なお、対応するmigrationファイルにtimestamps()のカラムがないとエラーになる
@@ -67,13 +67,13 @@ class ItemSeeder extends Seeder
 
 
         \App\Models\Item::create([
-            'user_id' => fake()->numberBetween($min=30,$max=100),//指定した範囲内の整数のどれか
+            'user_id' => '7',//指定した範囲内の整数のどれか
             'name' => '我が輩の万年筆',
             'type' => 2,
             'detail' =>'これじゃないとだめなやつ。誰だ出品したやつ。',
             // witchwatchみたいな英語として認識されない文字があるときは、code spell checkerでその単語を無視する設定をしないとエラーになるので注意
             'delete_flag' => 0,
-            'updated_by' => fake()->numberBetween($min=1,$max=10),
+            'updated_by' => 0,
             'created_at' => now(),//php artisan db:seed --class=ItemsSeeder（<--今回のseeder名）をひたすら繰り返す
             'updated_at' => now(),//現在の時刻を入れるメソッド
             //なお、対応するmigrationファイルにtimestamps()のカラムがないとエラーになる
@@ -81,13 +81,13 @@ class ItemSeeder extends Seeder
         ]);
 
         \App\Models\Item::create([
-            'user_id' => fake()->numberBetween($min=30,$max=100),//指定した範囲内の整数のどれか
+            'user_id' => '8',//指定した範囲内の整数のどれか
             'name' => '週刊少年マガジン',
             'type' => 6,
             'detail' =>'基本的に水曜日発売であるが、水曜日が祝日であるときにはその週の火曜日に発売する講談社の漫画雑誌、最近だと、赤羽骨子のボディーガードと女神のカフェテラスが面白い。あとジャンプに比べラブコメが多い傾向にある。',
             // witchwatchみたいな英語として認識されない文字があるときは、code spell checkerでその単語を無視する設定をしないとエラーになるので注意
             'delete_flag' => 0,
-            'updated_by' => fake()->numberBetween($min=1,$max=10),
+            'updated_by' => 1,
             'created_at' => now(),//php artisan db:seed --class=ItemsSeeder（<--今回のseeder名）をひたすら繰り返す
             'updated_at' => now(),//現在の時刻を入れるメソッド
             //なお、対応するmigrationファイルにtimestamps()のカラムがないとエラーになる
@@ -95,13 +95,13 @@ class ItemSeeder extends Seeder
         ]);
 
         \App\Models\Item::create([
-            'user_id' => fake()->numberBetween($min=30,$max=100),//指定した範囲内の整数のどれか
+            'user_id' => '9',//指定した範囲内の整数のどれか
             'name' => 'すごい釣り竿',
             'type' => 7,
             'detail' =>'どんな物も釣れる釣り竿',
             // witchwatchみたいな英語として認識されない文字があるときは、code spell checkerでその単語を無視する設定をしないとエラーになるので注意
             'delete_flag' => 0,
-            'updated_by' => fake()->numberBetween($min=1,$max=10),
+            'updated_by' => 1,
             'created_at' => now(),//php artisan db:seed --class=ItemsSeeder（<--今回のseeder名）をひたすら繰り返す
             'updated_at' => now(),//現在の時刻を入れるメソッド
             //なお、対応するmigrationファイルにtimestamps()のカラムがないとエラーになる
@@ -110,13 +110,13 @@ class ItemSeeder extends Seeder
 
 
         \App\Models\Item::create([
-            'user_id' => fake()->numberBetween($min=30,$max=100),//指定した範囲内の整数のどれか
+            'user_id' => '9',//指定した範囲内の整数のどれか
             'name' => '竹刀',
             'type' => 7,
             'detail' =>'剣道用具、個人的には、古刀型の柄長めのやつが使いやすい',
             // witchwatchみたいな英語として認識されない文字があるときは、code spell checkerでその単語を無視する設定をしないとエラーになるので注意
             'delete_flag' => 0,
-            'updated_by' => fake()->numberBetween($min=1,$max=10),
+            'updated_by' => 1,
             'created_at' => now(),//php artisan db:seed --class=ItemsSeeder（<--今回のseeder名）をひたすら繰り返す
             'updated_at' => now(),//現在の時刻を入れるメソッド
             //なお、対応するmigrationファイルにtimestamps()のカラムがないとエラーになる
