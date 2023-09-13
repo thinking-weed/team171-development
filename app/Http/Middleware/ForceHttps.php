0a49c104ce61d92b0081e472b32a4454c961b066
thinking-weed <133303560+thinking-weed@users.noreply.github.com>
2023-09-13 16:41:08 +0900

    ForceHttps.phpのif文をコメントアウト

--- app/Http/Middleware/ForceHttps.php
+++ app/Http/Middleware/ForceHttps.php
@@ -16,9 +16,9 @@ class ForceHttps
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (\App::environment(['production']) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
-            return redirect()->secure($request->getRequestUri());
-        }
+        // if (\App::environment(['production']) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
+        //     return redirect()->secure($request->getRequestUri());
+        // }
 
         return $next($request);
     }
