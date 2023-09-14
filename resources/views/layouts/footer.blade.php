9f7692bc8c88be4afc24fff074252ce236ed48fe
thinking-weed <133303560+thinking-weed@users.noreply.github.com>
2023-09-14 09:59:13 +0900

    参照の文字調節

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="ml-4 text-left text-sm sm:text-left sm:ml-0" style="color: white;font-weight:bold;">
-        <p class="reference">　背景画像は以下から引用したものをPhotoshopで<br>加工して利用しています。</p><br>
+        <p class="reference">　<br>背景画像は以下から引用したものをPhotoshopで<br>加工して利用しています。</p><br>
             <ul>
                 <li>・{!! 'unsplash(https://unsplash.com/ja)' !!}</li>
             </ul>
